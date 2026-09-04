0229a06a79c82af0bf4f32649878c80e283cf5d4
siliciumcat <siliciumcat@gmail.com>
2026-09-04 20:12:17 +0500

    feat: implement SalesMetric for revenue, category nad product analytics

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from src.processor import SalesDataProcessor
+from src.metrics import SalesMetrics
 
+# 1. Загрузка данных
 reading = SalesDataProcessor()
-
 df = reading.load_all_csv()
 
-print(df)
+# 2. Инициализация метрик с передачей df
+metrics = SalesMetrics(df)
+
+# 3. Расчёт аналитики
+total_revenue = metrics.get_total_revenue()
+sales_by_cat = metrics.get_sales_by_category()
+top_products = metrics.top_products()
+
+# 4. Вывод результатов
+print("=== Всего выручки ===")
+print(total_revenue)
+print("\n=== Продажи по категориям ===")
+print(sales_by_cat)
+print("\n=== Топ товаров ===")
+print(top_products)
---
+++ src/metrics.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+class SalesMetrics:
+    """class of Sales Metrics"""
+    
+    def __init__(self, df):
+        self.df = df
+
+    def get_total_revenue(self):
+        """Getting total revenue"""
+
+        return self.df["total_amount"].sum()
+
+    def get_sales_by_category(self):
+        """total revanue of the category"""
+
+        return self.df.groupby("category")["total_amount"].sum()
+
+    def top_products(self, n=5):
+        return (
+            self.df.groupby("product")["quantity"]
+            .sum()
+            .sort_values(ascending=False)
+            .head(n)
+        )
